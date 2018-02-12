85410dfc43be264fd79f10dcf9801117d7b5e245
diman <diman_nikolaev@mail.ru>
2018-02-12 19:01:58 +0300

    Add MiniMaxSum

---
+++ Hackerrank.Task/Hackerrank.Task/MiniMaxSum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackerrank.Task
+{
+    class MiniMaxSum
+    {
+        public static void miniMaxSum(int[] arr)
+        {
+            var count = arr.Length;
+            List<long> minMax = new List<long>();
+            for (int i = 0; i < count; i++)
+            {
+                List<long> sum = new List<long>();
+                for (int j = 0; j < count; j++)
+                {
+                    if (j != i)
+                    {
+                        sum.Add(arr[j]);
+                    }
+                }
+                minMax.Add(sum.Sum());
+            }
+            Console.WriteLine("{0} {1}", minMax.Min(), minMax.Max());
+
+            // как вариант
+            //long minSum = arr.Sum() - arr.Max();
+            //long maxSum = arr.Sum() - arr.Min();
+            //Console.WriteLine("{0} {1}", minSum, maxSum);
+        }
+    }
+}
--- Hackerrank.Task/Hackerrank.Task/Program.cs
+++ Hackerrank.Task/Hackerrank.Task/Program.cs
@@ -10,6 +10,12 @@ namespace Hackerrank.Task
     {
         static void Main(string[] args)
         {
+            // 1 2 3 4 5
+            // 10 14
+            string[] arr_temp = "1 2 3 4 5".Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            MiniMaxSum.miniMaxSum(arr);
+            Console.ReadKey();
         }
     }
 }
